f854ff71022e3d8f97b0cbf6661d20b9a8d91a56
Henrique <you@example.com>
2023-05-09 16:59:57 -0300

    homeHTML ajustada e renderizado parte das empresas

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,6 +1,6 @@
-import { getAllCategories } from './requests.js'
+import { getAllCategories, getAllCompanies } from './requests.js'
 
-// We are going to get all the categories from the API and render them on the select in the HTML document
+// We are going to get all the CATEGORIES from the API and render them on the HTML document
 async function renderSelect(){
     // We are getting an array with all the categories from the API and assigning them to the variables allCategories
     const allCategories = await getAllCategories()
@@ -8,7 +8,7 @@ async function renderSelect(){
     const select = document.querySelector('.companies__select')
 
     allCategories.forEach((category) => {
-        // We are going to create HTML element
+        // We are going to create the HTML element
         const option = document.createElement('option')
         
         // We are going to assign value to the element
@@ -22,4 +22,43 @@ async function renderSelect(){
         select.append(option)
     })
 }
+
+// We are going to get all the COMPANIES from the API and render them on the HTML document
+async function renderCompanies(){
+    // We are getting an array with all the categories from the API and assigning them to the variables allCategories
+    const allCompanies = await getAllCompanies()
+    // We are getting the 'select' from the HTML document
+    const container = document.querySelector('.companies__container')
+
+    allCompanies.forEach(async (company) => {
+        // We are going to create the HTML elements
+        const div = document.createElement('div')
+        const h3 = document.createElement('h3')
+        const p = document.createElement('p')
+        
+        // In the return of the getAllCompanies() function, we get an array. But, in this array, there is only 
+        // information about the category_id and we need the category Name. So, we call the getAllCategories() 
+        // function and filter the category with the same ID as the company (filteredCategory) 
+        const allCategories = await getAllCategories()
+        const categoryId = company.category_id
+        const filteredCategory = allCategories.filter((category) => category.id === categoryId)
+
+        // We are going to assign value to the element
+        h3.innerText = company.name
+        p.innerText = filteredCategory[0].name
+
+        // We are going to assign class and id to the element
+        div.classList = 'container__company'
+        h3.classList = 'container__company--h3'
+        p.classList = 'container__company--p'
+
+        // We are going to establish the hirarchy between the elements
+        container.append(div)
+        div.append(h3,p)
+    })
+}
+
+
+// Functions
 renderSelect()
+renderCompanies()
--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,7 +1,7 @@
 // We are going to define the URL base
 const baseUrl = 'http://localhost:3333'
 
-// We are going to get an array with all the sectors in the API (Alimetício, varejo ...)
+// We are going to get an array with all the CATEGORIES from the API (Alimetício, varejo ...)
 export async function getAllCategories(){
     const allCategories = await fetch(`${baseUrl}/categories/readAll`, {
         method: 'GET',
@@ -16,5 +16,22 @@ export async function getAllCategories(){
     })
 
     return allCategories
+}
+
+// We are going to get an array with all the COMPANIES from the API
+export async function getAllCompanies(){
+    const allCompanies = await fetch(`${baseUrl}/companies/readAll`, {
+        method: 'GET',
+    })
+    .then(async (res) => {
+        if(res.ok){
+            return res.json()
+        } else{
+            const response = await res.json()
+            console.log(response.message)
+        }
+    })
+
+    return allCompanies
 }
 
