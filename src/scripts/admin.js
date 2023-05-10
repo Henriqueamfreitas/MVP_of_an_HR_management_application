bd56160af60ac6760c496f49dce133fe7e003c09
Henrique <you@example.com>
2023-05-10 19:08:42 -0300

    renderizado departamentos no admin

--- src/scripts/admin.js
+++ src/scripts/admin.js
@@ -12,11 +12,11 @@ function redirectPage(button, path){
 }
 
 // We are going to create a function that opens the create category modal
-function handleCreateCategoryModal(){
-    const openButton = document.querySelector('.categories__top--button')
-    const modal = document.querySelector('.createCategorie__container')
-    const closeButton = document.querySelector('.createCategorie__form--closeButton')
-    const createButton = document.querySelector('.createCategorie__form--createButton')
+function handleCreateDepartmentModal(){
+    const openButton = document.querySelector('.department__top--button')
+    const modal = document.querySelector('.createDepartment__container')
+    const closeButton = document.querySelector('.createDepartment__form--closeButton')
+    const createButton = document.querySelector('.createDepartment__form--createButton')
 
     openButton.addEventListener('click', (event) => {
         event.preventDefault()
@@ -34,18 +34,73 @@ function closeModal(button, modal){
     })
 }
 
+
+
 const allDepartments = await getAllDepartments()
-console.log(allDepartments)
+async function createDepartmentCard(object){
+    // We are going to create the HTML elements
+    const card = document.createElement('div')
+    const cardDescriptions = document.createElement('div')
+    const h1 = document.createElement('h1')
+    const description = document.createElement('p')
+    const companyName = document.createElement('p')
+    
+    const cardButtons = document.createElement('div')
+    const seeDepartment = document.createElement('button')
+    const seeDepartmentImg = document.createElement('img')
+    const editDepartment = document.createElement('button')
+    const editDepartmentImg = document.createElement('img')
+    const excludeDepartment = document.createElement('button')
+    const excludeDepartmentImg = document.createElement('img')
+    
+    // Assigning classes and IDs to the HTML elements
+    card.classList = 'department__cards--card'
+    cardDescriptions.classList = 'card__descriptions'
+    h1.classList = 'card__descriptions--h1'
+    description.classList = 'card__descriptions--description'
+    companyName.classList = 'card__descriptions--companyName'
+    
+    cardButtons.classList = 'card__buttons'
+    seeDepartment.classList = 'card__buttons--seeDepartment'
+    seeDepartment.dataset.departmentId = object.id 
+    editDepartment.classList = 'card__buttons--editDepartment'
+    editDepartment.dataset.departmentId = object.id 
+    excludeDepartment.classList = 'card__buttons--excludeDepartment'
+    excludeDepartment.dataset.departmentId = object.id 
+    
+    // Getting the name of the company by the company id
+    const id = object.company_id
+    const company = await getCompanyById(id)
+    
+    // Assigning values to the HTML elements
+    h1.innerHTML = object.name
+    description.innerHTML = object.description
+    companyName.innerHTML = company.name
+    seeDepartmentImg.src = '../../images/editButon-Image.svg'
+    editDepartmentImg.src = '../../images/seeButon-Image.svg'
+    excludeDepartmentImg.src = '../../images/deleteButon-Image.svg'
 
-// console.log(id)
-// console.log(typeof(id))
-// console.log(JSON.parse(id))
-// console.log(typeof(id))
+    // Establishing the hierarchy between the elements
+    card.append(cardDescriptions, cardButtons)
+    cardDescriptions.append(h1, description, companyName)
+    cardButtons.append(seeDepartment, editDepartment, excludeDepartment)
+    seeDepartment.append(seeDepartmentImg)
+    editDepartment.append(editDepartmentImg)
+    excludeDepartment.append(excludeDepartmentImg)
 
+    return card
+}
 
-const company = await getCompanyById('0325382c-c3d0-41f7-b9ad-74e210522e96')
-console.log(company)
+function renderDepartment(array){
+    const cards = document.querySelector('.department__cards')
+    cards.innerHTML = ''
 
+    array.forEach(async (element) => {
+        const card = await createDepartmentCard(element)
+        cards.append(card)
+    })
+}
 
-handleCreateCategoryModal()
+handleCreateDepartmentModal()
 redirectPage(logoutButton, loginPath)
+renderDepartment(allDepartments)
