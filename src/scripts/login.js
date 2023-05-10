757eeb74e6eb55e4e032cfd8b8bd60f2ccdf8073
Henrique <you@example.com>
2023-05-10 08:20:04 -0300

    página html de login ok

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -3,11 +3,14 @@ const homeButton = document.querySelector('.header__buttons--home')
 const homePath = '/src/home.html'
 const registerPath = '/src/pages/register.html'
 const registerButton = document.querySelector('.header__buttons--register')
+const formRegisterButton = document.querySelector('.main__form--registerButton')
 function redirectPage(button, path){
-    button.addEventListener('click', ()=>{
+    button.addEventListener('click', (event)=>{
+        event.preventDefault()
         location.replace(path)
     })
 }
 
 redirectPage(homeButton, homePath)
 redirectPage(registerButton, registerPath)
+redirectPage(formRegisterButton, registerPath)
