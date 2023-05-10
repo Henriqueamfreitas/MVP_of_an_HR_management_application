b5800e1a2237b1dc6b94e861e29a9a15e156920c
Henrique <you@example.com>
2023-05-10 11:41:47 -0300

    botão de logout admin e user HTML funcionando

---
+++ src/scripts/admin.js
@@ -0,0 +1,16 @@
+
+// We are going to edit the logout, redirect to the login html page and clear the localStorage
+const logoutButton = document.querySelector('.header__buttons--logout')
+const loginPath = '/src/pages/login.html'
+function redirectPage(button, path){
+    button.addEventListener('click', (event)=>{
+        event.preventDefault()
+        localStorage.clear()
+        location.replace(path)
+    })
+}
+
+
+
+
+redirectPage(logoutButton, loginPath)
